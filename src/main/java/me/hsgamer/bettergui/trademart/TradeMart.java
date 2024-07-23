a5ed45121dcc0f5deaa2f17af753d1a76fcb1357
hsgamer <huynhqtienvtag@gmail.com>
2024-07-23 13:33:52 +0700

    update

--- src/main/java/me/hsgamer/bettergui/trademart/TradeMart.java
+++ src/main/java/me/hsgamer/bettergui/trademart/TradeMart.java
@@ -12,7 +12,7 @@ public final class TradeMart implements Expansion, GetPlugin {
 
     @Override
     public void onEnable() {
-        AdapterLoader.init();
+        AdapterLoader.init(getPlugin());
 
         MenuBuilder.INSTANCE.register(TradeMenu::new, "trade");
 
